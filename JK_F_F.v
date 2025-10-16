1da000a9bbc1b87c9b9d7bf25ac68c2259ea821c
Janani saraswathi <saraswathijanani4@gmail.com>
2025-10-16 15:23:08 +0530

    Add files via upload

---
+++ JK_F_F.v
@@ -0,0 +1,13 @@
+module JK_F_F (
+    input J, K, clk,       // Inputs: J, K, Clock
+    output reg Q           // Output: Q
+);
+    always @(posedge clk) begin
+        case ({J, K})
+            2'b00: Q <= Q;         // No change
+            2'b01: Q <= 1'b0;      // Reset
+            2'b10: Q <= 1'b1;      // Set
+            2'b11: Q <= ~Q;        // Toggle
+        endcase
+    end
+endmodule
